a34b39bac082a85e268ee51d158aea9cf18170e2
Santiago Prada <sanprabet@gmail.com>
2020-09-02 00:28:57 -0500

    Updates

---
+++ precache-manifest.626b7d04c5d96a5c5ba4e01105a74166.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9045e8bdcc974e70c9d6ce133d07728",
+    "url": "/quark-courses/index.html"
+  },
+  {
+    "revision": "73f46bd29c1cbfab6ecc",
+    "url": "/quark-courses/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b08ab8fcf1a2b9b79f96",
+    "url": "/quark-courses/static/css/main.4bda59c0.chunk.css"
+  },
+  {
+    "revision": "73f46bd29c1cbfab6ecc",
+    "url": "/quark-courses/static/js/2.da811a71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quark-courses/static/js/2.da811a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08ab8fcf1a2b9b79f96",
+    "url": "/quark-courses/static/js/main.a8994754.chunk.js"
+  },
+  {
+    "revision": "721568c6c4915efb6687",
+    "url": "/quark-courses/static/js/runtime-main.5afdc4ba.js"
+  },
+  {
+    "revision": "8770db2abf864ec682fa8f3f56bfb897",
+    "url": "/quark-courses/static/media/HeroBanner.8770db2a.png"
+  }
+]);
