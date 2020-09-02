6268bbc50c6e441cbccf7c85b83a87e19a7c63e2
Santiago Prada <sanprabet@gmail.com>
2020-09-02 00:31:14 -0500

    Se creo el Layout con la barra de navegación y el hero de la landing page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
 
-const container = document.getElementById('root')
+const container = document.getElementById('content')
 
-ReactDOM.render(<App />, container );
+ReactDOM.render(<App />, container);
 
