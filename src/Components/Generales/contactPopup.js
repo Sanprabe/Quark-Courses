4fb609e1b630bcaef314d675bc14cfaadeee4016
Santiago Prada <sanprabet@gmail.com>
2020-09-20 14:44:44 -0500

    Se centró el modal del video

--- src/Components/Generales/contactPopup.js
+++ src/Components/Generales/contactPopup.js
@@ -10,33 +10,33 @@ import './styles/contactPopup.css'
 export default function ContactPoup(props) {
     return (
         <Modal isOpen={props.modalIsOpen} shouldCloseOnOverlayClick={false} onRequestClose={props.closeModal} className='modalContact' overlayClassName="Overlay">
-                        <div className="modalContact__header">
-                            <button onClick={props.closeModal} className="modalContact__header-button">
-                                <img className="modalContact__header-logo" src={CloseLogo} alt="closeLogo"/>
-                            </button>
-                        </div>
-                        <div className="modalContact__info">
-                            <div className="modalContact__title">LAS MEJORES CLASES AL MEJOR PRECIO</div>
-                            <div className="modalContact__text">Contactanos en cualquiera de estos medios</div>
-                            <div className="modalContact__link-container">
-                                <a href='https://wa.link/bt4129' className="deco-none" target='blank'>
-                                    <div className='modalLink'>
-                                        <div className="modalLink__logo">
-                                        <img className='modalLink__logo-png' src={WhatsappLogo} alt="Whatsapp Logo"/>
-                                        </div>
-                                        {/* <div className="modalLink__text">Whattsap</div> */}
-                                    </div>
-                                </a>
-                                <a href="mailto:sanprabet@gmail.com?subject=Clases%20Online%20Efectivas%20&body=Deseo conocer más sobre las tutorias personalizadas de Quark Courses" className="deco-none" target='blank'>
-                                    <div className='modalLink'>
-                                        <div className="modalLink__logo">
-                                            <img className='modalLink__logo-png' src={GmailLogo} alt="Email Logo"/>
-                                        </div>
-                                        {/* <div className="modalLink__text">E-mail</div> */}
-                                    </div>
-                                </a>
+                <div className="modalContact__header">
+                    <button onClick={props.closeModal} className="modalContact__header-button">
+                        <img className="modalContact__header-logo" src={CloseLogo} alt="closeLogo"/>
+                    </button>
+                </div>
+                <div className="modalContact__info">
+                    <div className="modalContact__title">LAS MEJORES CLASES AL MEJOR PRECIO</div>
+                    <div className="modalContact__text">Contactanos en cualquiera de estos medios</div>
+                    <div className="modalContact__link-container">
+                        <a href='https://wa.link/bt4129' className="deco-none" target='blank'>
+                            <div className='modalLink'>
+                                <div className="modalLink__logo">
+                                    <img className='modalLink__logo-png' src={WhatsappLogo} alt="Whatsapp Logo"/>
+                                </div>
+                                {/* <div className="modalLink__text">Whattsap</div> */}
                             </div>
-                        </div>
-                </Modal>
+                        </a>
+                        <a href="mailto:sanprabet@gmail.com?subject=Clases%20Online%20Efectivas%20&body=Deseo conocer más sobre las tutorias personalizadas de Quark Courses" className="deco-none" target='blank'>
+                            <div className='modalLink'>
+                                <div className="modalLink__logo">
+                                    <img className='modalLink__logo-png' src={GmailLogo} alt="Email Logo"/>
+                                </div>
+                                {/* <div className="modalLink__text">E-mail</div> */}
+                            </div>
+                        </a>
+                    </div>
+                </div>
+        </Modal>
     )
 }
